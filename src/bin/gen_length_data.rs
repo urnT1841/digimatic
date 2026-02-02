7ab2d6e1f8a7619064049e90af332d1a69238a06
urnT1841 <260836856+urnT1841@users.noreply.github.com>
2026-02-02 11:06:53 +0900

    feat: :sparkles: 統計値の計算，出力機能を追加

---
+++ src/bin/gen_length_data.rs
@@ -0,0 +1,88 @@
+
+//!
+//!
+//! 
+//! 
+//! 
+//! 
+//! 
+//! 
+//! 
+
+
+use rand::Rng;
+
+#[derive(Debug,Default)]
+struct StatVal {
+    count:usize,
+    mean:f64,
+    max:f64,
+    min:f64,
+    std_sigma:f64,
+}
+
+const MAX_COUNT:usize= 1000;
+
+fn main() {
+
+    let mut rng = rand::rng();
+    let mut rnd_vec = Vec::with_capacity(MAX_COUNT);
+
+    for _ in 1..=MAX_COUNT {
+        // 長さ測定値を生成
+        let val: f64 = rng.random_range(0.02..=100.0);
+        rnd_vec.push(val);
+    
+    }
+    let stats = cal_stats(&rnd_vec);
+    print_stat_res(&stats);
+
+
+}
+
+///
+/// 生成した乱数の統計処理
+///
+/// 
+fn cal_stats ( rvec: &Vec<f64>) -> StatVal {
+    let mut s: StatVal = StatVal::default(); // 初期化
+    s.min = f64::MAX;
+
+    if rvec.is_empty() { // 受け取ったVecが空なら初期化した統計値構造体を返す
+        return s;
+    }
+
+    // 統計値算出
+    let mut sum:f64 = 0.0;
+    for &val in rvec {
+        s.max = s.max.max(val);
+        s.min = s.min.min(val);
+        s.std_sigma = 100.;
+        sum += val;
+    }
+        s.count = rvec.len();
+        s.mean = sum / s.count as f64;
+
+    // 分散・標準偏差を求める（2回目のループ：中心化して計算）
+    let variance: f64 = rvec.iter()
+        .map(|&x| (x - s.mean).powi(2)) // (x - 平均)^2
+        .sum::<f64>() / rvec.len() as f64;
+    
+    s.std_sigma = variance.sqrt(); // 標準偏差確定！
+
+    s
+
+} 
+
+
+///
+///  結果表示
+/// 
+fn print_stat_res( s_result:&StatVal ) {
+
+    //結果表示
+    println!("平均:     {:.3}", s_result.mean);
+    println!("最大:     {:.3}", s_result.max);
+    println!("最小:     {:.3}", s_result.min);
+    println!("標準偏差: {:.3}", s_result.std_sigma);
+}
