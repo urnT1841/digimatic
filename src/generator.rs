21334a5811499c51936406b4a2eb742a663e7164
urnT1841 <260836856+urnT1841@users.noreply.github.com>
2026-02-05 08:18:57 +0900

    refactor: :art: generatorでより実機っぽい 値を生成するように変更

--- src/generator.rs
+++ src/generator.rs
@@ -1,13 +1,17 @@
 //!
+//! ノギス測定データっぽい値の出力器
 //!
-//!
-//!
+//!  生成範囲：0.01mm ~ 150.00mm
+//!  備考：乱数で生成。実際に送られてくるような近い値が来る的な機能はなし
+//! 
 
 use rand::Rng;
 
 pub fn generator() -> f64 {
-    // 生成する値は Caliperの測定値を模して 0.02mm ~ 100.0 mm
+    // 生成する値は Caliperの測定値を模して 0.01mm ~ 100.0 mm
 
     let mut rng = rand::rng();
-    rng.random_range(0.02..=100.0)
+    let raw = rng.random_range(1 ..= 150_000);
+    let sim_data  = raw as f64 / 100.0;
+    sim_data
 }
