289fc3324b8e1df52fc5fd0227022026bcb21e69
urnT1841 <260836856+urnT1841@users.noreply.github.com>
2026-02-16 16:39:28 +0900

    feat: :sparkles: バイナリ受信機能を CDCreceiver にimplして隠蔽

--- pc_tool/src/lib.rs
+++ pc_tool/src/lib.rs
@@ -7,7 +7,9 @@ pub mod frame;
 pub mod frame_array_builder;
 pub mod generator;
 pub mod port_prepare;
-pub mod receiver;
 pub mod scanner_of_pico_connection;
 pub mod sender;
 pub mod validater_rx_frame;
+
+// リファクタリングの結果，使わなくなった
+//pub mod receiver;
