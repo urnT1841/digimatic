53b78c7312d4b24722171b450312ad42f999ce90
urnT1841 <260836856+urnT1841@users.noreply.github.com>
2026-02-19 15:54:34 +0900

    feat: 実機待ち受けにも無限待ち受けとタイムアウト処理を実装

--- pc_tool/src/execute_communicate.rs
+++ pc_tool/src/execute_communicate.rs
@@ -1,57 +1,86 @@
 //!
 //! 実際に外部機器(pico)と通信して処理する
-//! 
+//!
 
-use serialport::SerialPort;
-use std::time::Duration;
-use std::fs::{File,OpenOptions};
 use csv::{Writer, WriterBuilder};
+use serialport::SerialPort;
+use std::fs::{File, OpenOptions};
+use std::io::{self, Write};
+use std::time::Duration;
 
-use crate::scanner_of_pico_connection::find_pico_port;
-use crate::validater_rx_frame::parse_rx_frame;
 use crate::communicator::CdcReceiver;
 use crate::logger::*;
+use crate::scanner_of_pico_connection::find_pico_port;
+use crate::validater_rx_frame::parse_rx_frame;
 
+///
+/// pico 実機を探して接続，USB-CDCで待ち受けデータ受信
+///
 pub fn run_actual_loop() -> Result<(), Box<dyn std::error::Error>> {
-    // picoが接続されているポートを走査
-    // 見つからなかったらpanicで終わる。 // TODO: 後でエラーハンドリング実装すること
-    let pico_port_path = find_pico_port()?;
-    let rx_port = open_pico_port(&pico_port_path)?;
-    let mut rx_receiver = CdcReceiver::new(rx_port);
-
-    // 保存用にライター準備
-    let mut rx_wtr = create_log_writer("rx_log.csv")?;
-    let mut m_wtr = create_log_writer("measurement.csv")?;
-
+    let mut pico_waiting = 0;
+    //pico待ち受けループ
     loop {
-        match rx_receiver.read_str_measurement() {
-            Ok(data) => {
-                if data.is_empty() {
-                    continue;
-                }
+        // 待ち受け時間制限 10分 600s で設定
+        if pico_waiting > 600 {
+            println!("タイムアウト： 待ち受けを終了します。");
+            break Ok(());
+        }
 
-                // 受信記録記録を生成して記録
-                //  コンストラクタと .save_flash() メソッド を impl
-                RxDataLog::new(&data).save_flush(&mut rx_wtr)?;  // エラーの扱い注意
+        print!("\rpicoを探しています。{}秒 ", pico_waiting);
+        io::stdout().flush().unwrap();
 
-                // rx文字列(フレーム)のバリデーション
-                match parse_rx_frame(&data) {
-                    Ok(measurement) => {
-                        let val_f64 = measurement.to_f64();
-                        MeasurementLog::new(val_f64).save_flush(&mut m_wtr)?;    // エラーの扱い注意
-                        println!("{} {:?} : ", measurement.raw_val, measurement.unit);
-                        print_rx_decode_result(&data, val_f64);
-                    }
-                    Err(e) => {
-                        eprintln!("データ異常（パース失敗）: {} | 原因: {}", data, e);
-                    }
-                }
+        // picoを探す
+        let pico_port_path = match find_pico_port() {
+            Ok(path) => path,
+            Err(_) => {
+                std::thread::sleep(Duration::from_secs(1));
+                pico_waiting += 1;
+                continue;
             }
-            // タイムアウト時は何もしない
-            Err(ref e) if e.kind() == std::io::ErrorKind::TimedOut => (),
-            // それ以外のエラー
-            Err(e) => {
-                eprintln!("受信エラー {}", e);
+        };
+        // 見つかったのでリセット
+        pico_waiting = 0;
+
+        // port open
+        let rx_port = match open_pico_port(&pico_port_path) {
+            Ok(port) => port,
+            Err(_) => continue,
+        };
+        let mut rx_receiver = CdcReceiver::new(rx_port);
+        // 保存用にライター準備
+        let mut rx_wtr = create_log_writer("rx_log.csv")?;
+        let mut m_wtr = create_log_writer("measurement.csv")?;
+
+        loop {
+            match rx_receiver.read_str_measurement() {
+                Ok(data) => {
+                    if data.is_empty() {
+                        continue;
+                    }
+
+                    // 受信記録記録を生成して記録
+                    //  コンストラクタと .save_flash() メソッド を impl
+                    RxDataLog::new(&data).save_flush(&mut rx_wtr)?; // エラーの扱い注意
+
+                    // rx文字列(フレーム)のバリデーション
+                    match parse_rx_frame(&data) {
+                        Ok(measurement) => {
+                            let val_f64 = measurement.to_f64();
+                            MeasurementLog::new(val_f64).save_flush(&mut m_wtr)?; // エラーの扱い注意
+                            println!("{} {:?} : ", measurement.raw_val, measurement.unit);
+                            print_rx_decode_result(&data, val_f64);
+                        }
+                        Err(e) => {
+                            eprintln!("データ異常（パース失敗）: {} | 原因: {}", data, e);
+                        }
+                    }
+                }
+                // タイムアウト時は何もしない
+                Err(ref e) if e.kind() == std::io::ErrorKind::TimedOut => (),
+                // それ以外のエラー
+                Err(e) => {
+                    eprintln!("受信エラー {}", e);
+                }
             }
         }
     }
@@ -59,12 +88,9 @@ pub fn run_actual_loop() -> Result<(), Box<dyn std::error::Error>> {
 
 ///
 /// ライター生成
-/// 
+///
 fn create_log_writer(path: &str) -> Result<Writer<File>, Box<dyn std::error::Error>> {
-    let file = OpenOptions::new()
-        .create(true)
-        .append(true)
-        .open(path)?;
+    let file = OpenOptions::new().create(true).append(true).open(path)?;
 
     Ok(WriterBuilder::new().has_headers(false).from_writer(file))
 }
