f008a21ef1f0f985b3d17e8cfe268bc922a75a59
urnT1841 <260836856+urnT1841@users.noreply.github.com>
2026-02-19 10:10:02 +0900

    refactor: frame.rsに押し込んでいた通信機能や ロギング機能を別ファイルに分離。それに伴う調整

--- pc_tool/src/lib.rs
+++ pc_tool/src/lib.rs
@@ -1,13 +1,14 @@
 //!
 //! lib化
 //!
-//!
 
 pub mod frame;
 pub mod scanner_of_pico_connection;
 pub mod sim;
 pub mod validater_rx_frame;
 pub mod execute_communicate;
+pub mod communicator;
+pub mod logger;
 
 // リファクタリングの結果，使わなくなった
 //pub mod receiver;
