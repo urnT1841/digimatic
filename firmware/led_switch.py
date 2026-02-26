30dbaf7f3bbc29975741ceb7ba99fd3235f31365
urnT1841 <260836856+urnT1841@users.noreply.github.com>
2026-02-27 08:17:43 +0900

    style: :art: RP2040のLEDは負論理，DataPinは正論理だったので混じらないようLEDに関してはLED_ON/LED_OFFを負論理で定義

--- firmware/led_switch.py
+++ firmware/led_switch.py
@@ -2,14 +2,14 @@
 
 import machine
 
-from pin_difinitions import ON,OFF
-
-
 # LED設定
 green_led = machine.Pin(16, machine.Pin.OUT)
 red_led = machine.Pin(17, machine.Pin.OUT)
 blue_led = machine.Pin(25, machine.Pin.OUT)
 
+# XAIO RP2040のUserLEDは負論理
+LED_ON = 0
+LED_OFF = 1
 
 def led_switch(r = OFF , g = OFF, b = OFF):
     """   xaio に搭載されている3つのLD制御    
