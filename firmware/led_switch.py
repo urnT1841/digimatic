d558a987daa35e0364c219071e84f15f28d2cd66
urnT1841 <260836856+urnT1841@users.noreply.github.com>
2026-02-27 15:06:53 +0900

    feat: pico側に state machine 導入。ただし実際の処理は未実装

--- firmware/led_switch.py
+++ firmware/led_switch.py
@@ -1,6 +1,7 @@
 
 
 import machine
+from micropython import const
 
 # LED設定
 green_led = machine.Pin(16, machine.Pin.OUT)
@@ -8,10 +9,10 @@ red_led = machine.Pin(17, machine.Pin.OUT)
 blue_led = machine.Pin(25, machine.Pin.OUT)
 
 # XAIO RP2040のUserLEDは負論理
-LED_ON = 0
-LED_OFF = 1
+LED_ON = const(0)
+LED_OFF = const(1)
 
-def led_switch(r = OFF , g = OFF, b = OFF):
+def led_switch(r = LED_OFF , g = LED_OFF, b = LED_OFF):
     """   xaio に搭載されている3つのLD制御    
     """
 
