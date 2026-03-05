65323b032e89ce27f89a818473cdb25d2294bddc
urnT1841 <260836856+urnT1841@users.noreply.github.com>
2026-03-06 08:25:17 +0900

    style: Typo修正

--- firmware/test_tools/busy-loop_wait.py
+++ firmware/test_tools/busy-loop_wait.py
@@ -18,6 +18,7 @@ BIN_FRAME_LENGTH = 52   # デジマチックのフレームは 52bit
 def receive_busy(bits_buffer):
     """
     Busy-loop でクロック同期受信するシンプル版
+    process_receive()と入れ替えて使う
     - Requestは事前に送信済み、または最初に送る
     - Pythonの処理遅延を極力排除
     - bits_buffer: 長さ BIN_FRAME_LENGTH のリスト
@@ -31,14 +32,19 @@ def receive_busy(bits_buffer):
     # 最初のClock立下り待ち
     while _clk.value() == ON:
         pass
+    
+    #ここで1Bit目読んだほうが良いか？
+    #まずは下記を試して2重に持って行ったりするようならここで読む。
+    # bits_buffer[0] = _data.value()
 
     # ビット受信ループ
+    # 1bit目を上で読む場合 ranage(1,BIN~)とすること
     for i in range(BIN_FRAME_LENGTH):
         # CLOCK High 待ち
         while _clk.value() == OFF:
             pass
         # CLOCK Low 待ち
-        while _clk.value() == OFF:
+        while _clk.value() == ON:
             pass
         # データ読み取り
         bits_buffer[i] = _data.value()
