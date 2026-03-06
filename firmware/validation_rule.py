c91273d45ff3a488c8db12d4d12ef0db4433405f
urnT1841 <260836856+urnT1841@users.noreply.github.com>
2026-03-06 12:14:28 +0900

    refactor: StateMachineでの処理を state_process.pyに追い出した
    処理の別ファイル化に伴い細かい調整とTypo(ファイル名含む)修正

--- firmware/validation_rule.py
+++ firmware/validation_rule.py
@@ -1,3 +1,19 @@
+
+# StateMachine 状態定義
+STATE_IDLE = 0
+STATE_REQUEST = 1
+STATE_RECEIVE = 2
+STATE_VALIDATE = 3
+STATE_ERROR = 4
+
+#エラー定義
+ERR_NONE = 0
+ERR_TIMEOUT = 1  # 信号が来ない
+ERR_READ    = 2  # クロックが途中で途切れた、物理的ノイズ  # TODO: 返す部分は未実装
+ERR_DECODE  = 3  # バリデーション（FFFFヘッダ等）失敗
+
+
+
 # バリデーションルール
 # d6~d11 (index 5~10) はすべてBCD (0-9)
 CHECK_RULES = {
