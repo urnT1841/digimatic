1a793f96b1bbeb681da4ba0cac0bc4e5f3efcef2
urnT1841 <260836856+urnT1841@users.noreply.github.com>
2026-03-08 00:37:45 +0900

    refactor: 受信バッファをリストからbytearrayへ変更。ほかdebug printを削除
    目盛りの効率化のため list から bytearrayに変更。デバックプリントを削除。 state_idelで何もしないように変更

--- firmware/decoder.py
+++ firmware/decoder.py
@@ -8,27 +8,51 @@ from validation_rule import ERR_DECODE, ERR_NONE, ERR_READ, ERR_TIMEOUT
 BIN_FRAME_LENGTH = 52   # デジマチックのフレームは 52bit
 
 
-def validate(nib13_frame):
-    print("DEBUG: start validate")
-    for i, nib in enumerate(nib13_frame):
-        val = to_int(nib)
+def validator(bits_buffer):
+    """
+    受信したbit列の検証とBCD変換
+    リスト内包記法による重いスライスやリスト作成などの処理を行わないようにした版   
+    """
+
+    REQUIRED_BIT_LENGTH = 52  # 受信したビット列の長さ
+    NIBBLE = 4
+    NIBBLE_CHUNK_COUNT = REQUIRED_BIT_LENGTH // NIBBLE   # デジマチックフレーム の nibble の数 4bit x 13 = 52
+
+    # 長さチェック（raiseを排除）
+    if len(bits_buffer) != REQUIRED_BIT_LENGTH:
+        return STATE_ERROR, ERR_DECODE
+    
+    results = []
+    
+    for i in range(NIBBLE_CHUNK_COUNT):
+        base = i * NIBBLE
+        val = (bits_buffer[base + 3] << 3) | (bits_buffer[base + 2] << 2) | \
+              (bits_buffer[base + 1] << 1) | bits_buffer[base]
         
-        # 個別ルール
+        # 個別ルールチェック
         if i in CHECK_RULES:
             if not CHECK_RULES[i](val):
                 return None
 
-        # BCD領域(d6~d11)の共通チェック
+        # BCDデータ領域チェック
+        # マジックナンバーの意味は validation_ruleみること
         elif 5 <= i <= 10:
             if not (0 <= val <= 9):
                 return None
+        
+        # ALL 1 は F とする
+        results.append("F" if val == 15 else str(val))
+    
+    return "".join(results)
 
-    return True
 
+
+# ----------------------------------------------------------------------
+# 以下の関数は使わなくなったが,残しておく
 #
 # pico では使わない。もっと強力なマイコンならいけるか？
 # いやNGって決まったわけじゃないけど
-def validator_list_Comprehension(bit_list):
+def validator_list_Comprehension(bits_buffer):
     """
     受信した52ビットの検証
     digimaticの意味付けに戻す
@@ -43,62 +67,23 @@ def validator_list_Comprehension(bit_list):
     REQUIRED_BIT_LENGTH = 52
 
     #長さチェック    
-    if len(bit_list) != REQUIRED_BIT_LENGTH:
+    if len(bits_buffer) != REQUIRED_BIT_LENGTH:
         return STATE_ERROR, ERR_DECODE
     
     # 妥当な長さであったので 13個のnibble にスライス
     # ここでlsb -> msb に直しておく。詰めるのはタプル(中身をいじらないという意思)
     # リスト内包記法を使っているのでもしかしたら重いかもということでこれはやめる
-    nib13_frame = [tuple(bit_list[i*4 : i*4+4][::-1]) for i in range(13)]
+    nib13_frame = [tuple(bits_buffer[i*4 : i*4+4][::-1]) for i in range(13)]
 
     # ここから本番バリデーションして，BCD変換
     # 実際のチェックは validate で実施
     if validate(nib13_frame):
         # 通ったのでnibble -> bcd, かつ13Byteの連続した文字列
-        print("DEBUG: complete validate. make digi-strings")
         validated = to_bcd_output(nib13_frame)
         return validated
     else:
         # バリテーション失敗
-        print("DEBUG: false validae")
         return None
-
-
-def validator(bit_list):
-    """
-    受信したbit列の検証とBCD変換
-    リスト内包記法による重い スライスやリスト作成などの処理を行わないようにした版   
-    """
-
-    REQUIRED_BIT_LENGTH = 52
-    
-    # 長さチェック（raiseを排除）
-    if len(bit_list) != REQUIRED_BIT_LENGTH:
-        return STATE_ERROR, ERR_DECODE
-    
-    results = []
-    
-    for i in range(13):
-        base = i * 4
-        # あの「一行」のビット演算
-        val = (bit_list[base + 3] << 3) | (bit_list[base + 2] << 2) | \
-              (bit_list[base + 1] << 1) | bit_list[base]
-        
-        # 個別ルールチェック
-        if i in CHECK_RULES:
-            if not CHECK_RULES[i](val):
-                return None
-
-        # BCDデータ領域チェック
-        elif 5 <= i <= 10:
-            if not (0 <= val <= 9):
-                return None
-        
-        results.append("F" if val == 15 else str(val))
-    
-    return "".join(results)
-
-
 
 
 def to_bcd_output(nib13_list):
@@ -108,7 +93,7 @@ def to_bcd_output(nib13_list):
     return "".join( digi_frame_str)
 
 
-# 以下の関数はリスト内法表記にしたので使わなくなった
+
 # が，まだデバッグで使うかもなので残しておく
 def reverse_nibble(nib_list):
     """ lsb -> msb, 或いは msb -> lsb への並び替え
@@ -126,3 +111,18 @@ def to_int(nib):
     return (nib[0] << 3) | (nib[1] << 2) | (nib[2] << 1) | nib[3]
 
 
+def validate(nib13_frame):
+    for i, nib in enumerate(nib13_frame):
+        val = to_int(nib)
+        
+        # 個別ルール
+        if i in CHECK_RULES:
+            if not CHECK_RULES[i](val):
+                return None
+
+        # BCD領域(d6~d11)の共通チェック
+        elif 5 <= i <= 10:
+            if not (0 <= val <= 9):
+                return None
+
+    return True
