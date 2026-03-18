564ceee7f118959fb2de734c6dc950d3778f2bcf
urnT1841 <260836856+urnT1841@users.noreply.github.com>
2026-03-18 12:47:54 +0900

    feat: GUI コードを src いかに mv して本番向けとして採用。それに伴う cargo.toml 修正

--- pc_tool/src/gui_main.rs
+++ pc_tool/src/gui_main.rs
@@ -1,37 +1,38 @@
 use std::sync::mpsc::Receiver;
-use rand::Rng;
 use eframe::egui;
 
-struct MyAppData {
-    dice_value: u32,
-    receiver: Receiver<u32>, // 受信機を格納
+use digimatic::sim::generator::generator;
+
+struct DisplayApp {
+    measurement_data: f64,
+    receiver: Receiver<f64>, // 受信機を格納
 }
 
-impl MyAppData {
+impl DisplayApp {
     // 起動時に受信機を受け取るための専用の初期化関数
-    fn with_receiver(rx: Receiver<u32>) -> Self {
+    fn with_receiver(rx: Receiver<f64>) -> Self {
         Self {
-            dice_value: 0,
+            measurement_data: 0.0,
             receiver: rx,
         }
     }
 }
-impl eframe::App for MyAppData {
+impl eframe::App for DisplayApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         // データ受信チェック
         // try_recv() は「データがあれば取る、なければすぐ次へ」という動き
         // while を使うことで、溜まっているデータをすべて処理する
         while let Ok(new_val) = self.receiver.try_recv() {
-            self.dice_value = new_val;
+            self.measurement_data = new_val;
             ctx.request_repaint();
         }
 
         // 描画
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.vertical_centered(|ui| {
-                ui.label(egui::RichText::new("自動計測シミュレーション中").size(20.0));
+                ui.label(egui::RichText::new("計測中").size(20.0));
 
-                let val_mm = self.dice_value as f64 / 100.0;
+                let val_mm = self.measurement_data as f64 / 100.0;
                 ui.label(
                     egui::RichText::new(format!("{:.2} mm", val_mm))
                         .size(120.0)
@@ -46,22 +47,21 @@ fn main() -> eframe::Result {
     let options = eframe::NativeOptions::default();
 
     eframe::run_native(
-        "Digimatic Display v0.33",
+        "Digimatic Data Display v0.33",
         options,
         Box::new(|cc| {
             // 送受信窓口
-            let (tx, rx) = std::sync::mpsc::channel::<u32>();
+            let (tx, rx) = std::sync::mpsc::channel::<f64>();
             let ctx_for_thread = cc.egui_ctx.clone();
 
             // スレッド起動
             std::thread::spawn(move || {
                 loop {
-                    let val = big_num_dice();
-                    if tx.send(val).is_err() { break; }
-                    
-                    // GUI を起こす
+                    let val = generator();
+                    if tx.send(val).is_err() {
+                         break;
+                    }
                     ctx_for_thread.request_repaint();
-
                     std::thread::sleep(std::time::Duration::from_millis(1200));
                 }
             });
@@ -76,14 +76,7 @@ fn main() -> eframe::Result {
             cc.egui_ctx.set_fonts(fonts);
 
             // rxを送って窓開始
-            Ok(Box::new(MyAppData::with_receiver(rx)))
+            Ok(Box::new(DisplayApp::with_receiver(rx)))
         }),
     )
-}
-
-
-fn big_num_dice() -> u32 {
-    let mut rng = rand::rng();
-    let dice = rng.random_range(1..=150_00);
-    dice
 }
