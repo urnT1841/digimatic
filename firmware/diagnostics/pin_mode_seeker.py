5e73d3ff8bf18e3341eb849b81901719de2385e9
urnT1841 <260836856+urnT1841@users.noreply.github.com>
2026-03-24 16:08:21 +0900

    feat: :construction: ピン状態一覧取得の実装開始

--- firmware/diagnostics/pin_mode_seeker.py
+++ firmware/diagnostics/pin_mode_seeker.py
@@ -3,3 +3,9 @@
 import utils
 import pin_register
 
+
+##! Xiao の pin状態一覧ゲット
+def pins_state():
+    #これから実装
+    return 0
+
--- firmware/diagnostics/pin_register.py
+++ firmware/diagnostics/pin_register.py
@@ -7,6 +7,13 @@ from micropython import const
 ### 2.3 Processer subsystem
 ### 2.19 GPIO
 
+# GPIO pin と pin_no の対応
+MAP = {
+    "D0": 26, "D1": 27, "D2": 28, "D3": 29,
+    "D4": 6,  "D5": 7,  "D6": 0,  "D7": 1,
+    "D8": 2,  "D9": 4,  "D10": 3
+}
+
 
 # Base Addresses
 SIO_BASE        = const(0xd0000000)
@@ -23,3 +30,6 @@ PUE_BIT_SHIFT   = const(3) # Pull-up Enable
 PDE_BIT_SHIFT   = const(2) # Pull-down Enable
 SCHMITT_SHIFT   = const(1) # Schmitt Trigger (ノギス信号には重要！)
 
+
+def get_bit_pos(label):
+    return MAP.get(label)
