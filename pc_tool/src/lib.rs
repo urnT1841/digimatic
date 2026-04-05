a832e2de5968e40c496acfd2a65d7dbe36d1ebe9
urnT1841 <260836856+urnT1841@users.noreply.github.com>
2026-04-06 08:49:36 +0900

    refactor: ファイル名変更に伴う修正

--- pc_tool/src/lib.rs
+++ pc_tool/src/lib.rs
@@ -6,9 +6,6 @@ pub mod communicator;
 pub mod execute_communicate;
 pub mod frame;
 pub mod logger;
+pub mod parser;
 pub mod scanner;
 pub mod sim;
-pub mod validater;
-
-// リファクタリングの結果，使わなくなった
-//pub mod receiver;
