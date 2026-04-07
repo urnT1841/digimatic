4bf484799109823ac0a08b0f5dc6ec96db43bcf3
urnT1841 <260836856+urnT1841@users.noreply.github.com>
2026-04-07 10:49:09 +0900

    feat: 引数で actual Mode (ノギスデータ待ち受け) か Sim Mode を選択できるように(help表示はなし)

--- pc_tool/src/main.rs
+++ pc_tool/src/main.rs
@@ -6,13 +6,20 @@
 
 use digimatic::execute_communicate;
 use digimatic::sim::execute_sim;
+use std::env;
 
 fn main() {
-    const MODE: &str = "actual";
-
-    let result = match MODE {
-        "sim" => execute_sim::run_simmulation_loop(),
+    // 簡単な引数処理 sim or actual(default)
+    let args:Vec<String> = env::args().collect();
+    let mode = args.get(1).map(|s| s.as_str()).unwrap_or("actual");
+    
+    let result = match mode {
+        "sim" => {
+            println!("-- Simlation Mode -- ");
+            execute_sim::run_simmulation_loop()
+        },
         "actual" => {
+            println!("-- Actual Mode  --");
             // guiへの窓口 acutual_loopの引数で必要なので個々で生成
             let (tx, _rx) = std::sync::mpsc::channel::<f64>();
             execute_communicate::run_actual_loop(tx)
