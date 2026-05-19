f8dc447ceb2dbbb19d5f53b00e3953dc31bbb41f
urnT1841 <260836856+urnT1841@users.noreply.github.com>
2026-05-19 16:36:00 +0900

    feat: :coffin: Clean up dependencies crates

--- pc_tool/src/execute_communicate.rs
+++ pc_tool/src/execute_communicate.rs
@@ -3,14 +3,9 @@
 //!
 
 use csv::{Writer, WriterBuilder};
-use serialport::SerialPort;
 use std::fs::{File, OpenOptions};
-use std::io::{self, Write};
 use std::sync::mpsc::Sender;
-use std::time::Duration;
 
-use crate::communicator::CdcReceiver;
-use crate::communicator::MeasurementRead;
 use crate::config::ConsoleMode;
 use crate::errors::{CommError, DigimaticError, FrameParseError};
 use crate::frame::{DigimaticFrame, Measurement};
