db6d74b53e3bca61e6b3aae85cd5cb5b59608669
urnT1841 <260836856+urnT1841@users.noreply.github.com>
2026-05-30 19:07:57 +0900

    refactor(Type): :recycle: 送信フレームを TransFormt に修正

--- pc_tool/src/sim/execute_sim.rs
+++ pc_tool/src/sim/execute_sim.rs
@@ -9,10 +9,11 @@ use std::sync::mpsc::Sender;
 use crate::received_data_handler::FrameFormat;
 use crate::sim::frame_builder::build_simurator_payload;
 use crate::sim::generator;
+use crate::frame::TransportFrame;
 
 /// データ生成スレッド
 /// channel使ってreceiverに流し込む
-pub fn start_generator_thread(tx: Sender<Vec<u8>>, frame_mode: FrameFormat) {
+pub fn start_generator_thread(tx: Sender<TransportFrame>, frame_mode: FrameFormat) {
     std::thread::spawn(move || {
         loop {
             let val = generator::generator();
